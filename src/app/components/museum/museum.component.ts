604b2ae7788ce5f805904572ba1e7fd02b942a5f
RubenCosta96 <a12057@alunos.ipca.pt>
2024-06-10 17:54:07 +0100

    museum

--- src/app/components/museum/museum.component.ts
+++ src/app/components/museum/museum.component.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './museum.component.html',
-  styleUrl: './museum.component.scss'
+  styleUrl: './museum.component.scss',
 })
 export class MuseumComponent {
-
+  toggleSpeak() {
+    if (window.speechSynthesis.speaking) {
+      window.speechSynthesis.cancel();
+      document.querySelector('.btn-speak').textContent = 'Ouvir Texto';
+    } else {
+      var text = document.getElementById('museum-history-text').innerText;
+      var speech = new SpeechSynthesisUtterance(text);
+      window.speechSynthesis.speak(speech);
+      document.querySelector('.btn-speak').textContent = 'Parar de Ouvir';
+    }
+  }
 }
